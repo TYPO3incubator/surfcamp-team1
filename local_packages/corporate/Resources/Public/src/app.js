da289cfd99fe685a16158e7275245d32c3feae81
penczko <alex.penczko@digital-zombies.com>
2024-04-10 14:10:02 +0200

    [TASK] add bootstrap

--- local_packages/corporate/Resources/Public/src/app.js
+++ local_packages/corporate/Resources/Public/src/app.js
@@ -1,6 +1,6 @@
 import example from "./js/example";
 import general from "./js/general";
-
+import 'bootstrap/js/dist/carousel';
 document.addEventListener("DOMContentLoaded", function () {
     //general
     general()
