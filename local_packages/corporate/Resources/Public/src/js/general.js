9de9c3544b8c540747bcc8bd6079b47e0b4cae3c
Natascha Klein <natascha.klein@hdnet.de>
2024-04-11 17:09:49 +0100

    feat(header): Finish the Header
    - refactor the svg's to get different Logo-Colors
    -refactor the Desktop Height of the header
    
    Ref.: #11

--- local_packages/corporate/Resources/Public/src/js/general.js
+++ local_packages/corporate/Resources/Public/src/js/general.js
@@ -7,8 +7,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header-scroll").style.height = "82px";
     document.getElementById("base-header--logo-desktop").style.display = "none";
+    document.getElementById("base-header--logo-mobile").style.display = "flex";
   } else {
-    document.getElementById("header-scroll").style.height = "160px";
+    document.getElementById("header-scroll").style.height = "8rem";
     document.getElementById("base-header--logo-desktop").style.display = "flex";
+    document.getElementById("base-header--logo-mobile").style.display = "none";
   }
 }
