6659607c581896fac8d060e14d9f947edbba105f
Natascha Klein <natascha.klein@hdnet.de>
2024-04-11 15:24:14 +0100

    refactor(header-function): Outsource the function to make the header smaller
    Ref.: #0

--- local_packages/corporate/Resources/Public/src/js/general.js
+++ local_packages/corporate/Resources/Public/src/js/general.js
@@ -1,4 +1,14 @@
-function general(){
-}
 
-export default general;
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header-scroll").style.height = "82px";
+    document.getElementById("base-header--logo-desktop").style.display = "none";
+  } else {
+    document.getElementById("header-scroll").style.height = "160px";
+    document.getElementById("base-header--logo-desktop").style.display = "flex";
+  }
+}
