9ae39e0ea34e07aab62375c0a207ca2e036fda88
mohamedachrafmaktouf <mohamed.maktouf@digital-zombies.com>
2024-04-12 12:25:41 +0100

    [TASK] Remove outline from All elements

--- local_packages/corporate/Resources/Public/src/app.js
+++ local_packages/corporate/Resources/Public/src/app.js
@@ -6,8 +6,6 @@ import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/collapse';
 
 document.addEventListener("DOMContentLoaded", function () {
-    //general
-    general()
 
     //example
     const exampleElements = document.querySelectorAll(".exampleSection");
@@ -19,4 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
     for (const sizesElement of sizesElements) {
         sizes(sizesElement);
     }
+    general()
+
 });
