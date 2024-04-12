b6518271a266a3fb260e0a81e8fbe73821a5ffd5
Natascha Klein <natascha.klein@hdnet.de>
2024-04-12 14:41:46 +0100

    fix(header): Fix Header scroll with Ben
    Ref.: #9

--- local_packages/corporate/Resources/Public/src/js/general.js
+++ local_packages/corporate/Resources/Public/src/js/general.js
@@ -1,16 +1,13 @@
-
 window.onscroll = function () {
   scrollFunction()
 };
 
+scrollFunction();
+
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header-scroll").style.height = "82px";
-    document.getElementById("base-header--logo-desktop").style.display = "none";
-    document.getElementById("base-header--logo-mobile").style.display = "flex";
+  if (document.documentElement.scrollTop > 50) {
+    document.documentElement.classList.add("header-scroll");
   } else {
-    document.getElementById("header-scroll").style.height = "8rem";
-    document.getElementById("base-header--logo-desktop").style.display = "flex";
-    document.getElementById("base-header--logo-mobile").style.display = "none";
+    document.documentElement.classList.remove("header-scroll");
   }
 }
