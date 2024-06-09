390b229f5498394be56b917ce9280c1c8310ad6f
Cyrill Gsell <cgs@rtp.ch>
2024-06-09 20:03:36 +0200

    [BUGFIX] Fixing import error in general js module

--- local_packages/corporate/Resources/Public/src/js/general.js
+++ local_packages/corporate/Resources/Public/src/js/general.js
@@ -1,13 +1,18 @@
-window.onscroll = function () {
-  scrollFunction()
-};
+function general() {
+  window.onscroll = function () {
+    scrollFunction()
+  };
 
-scrollFunction();
+  scrollFunction();
 
-function scrollFunction() {
-  if (document.documentElement.scrollTop > 50) {
-    document.documentElement.classList.add("header-scroll");
-  } else {
-    document.documentElement.classList.remove("header-scroll");
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 50) {
+      document.documentElement.classList.add("header-scroll");
+    } else {
+      document.documentElement.classList.remove("header-scroll");
+    }
   }
 }
+
+export default general;
+
